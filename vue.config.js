92526a2463d0eba63d9b1bbdd1b61f92e977386d
genevarivas <geneva.rivas@student.passerellesnumeriques.org>
2021-02-10 14:44:31 +0800

    updates

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         config
             .plugin('html')
             .tap(args => {
-                args[0].title = "My Vue App";
+                args[0].title = "Quizzone";
                 return args;
             })
     }
