cda586a72ba84c228633d995f11b0ee9293b939d
acerapa <projectdev751@gmail.com>
2023-08-04 22:32:48 +0800

    Create new branch

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      height: {
+        'calc-20': 'calc(100vh - 5rem)',
+      }
+    },
+  },
+  plugins: [],
+}
+
