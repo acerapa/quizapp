c140bb1ff1b59069ffb17926862b562072939146
haparece <harvey@fulloutsoftware.com>
2023-08-29 18:41:31 +0800

    update side nav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -7,7 +7,7 @@
             </div>
             <div :class="['flex items-center hover:bg-[#5da783] rounded p-1 mt-1', {'bg-[#5da783]': nav.isActive}]" v-for="(nav, index) in navigations" :key="index">
                 <RouterLink :to="{ name: nav.name }" :class="['text-gray-600 hover:text-white w-[100%]', {'text-white': nav.isActive}]">
-                    <img :src="`src/assets/${nav.icon}`" class="w-10 h-10 mr-5 invert inline cursor-pointer" alt="Test"> <span
+                    <img :src="nav.icon" class="w-10 h-10 mr-5 invert inline cursor-pointer" alt="Test"> <span
                         class="cursor-pointer">{{ nav.text }}</span>
                 </RouterLink>
             </div>
@@ -20,6 +20,11 @@ import { RouterLink, useRoute } from 'vue-router';
 import { useAuthStore } from '../stores/auth';
 import { onMounted, ref } from 'vue';
 
+// icons
+import dashboard from '@/assets/dashboard.png'
+import test from '@/assets/test.png'
+import folder from '@/assets/folder.png'
+
 const authStore = useAuthStore();
 const username = ref('')
 const route = useRoute();
@@ -28,7 +33,7 @@ const navigations = ref([
     {
         name: 'dashboard',
         text: 'Dashboard',
-        icon: 'dashboard.png',
+        icon: dashboard,
         names: [
             'dashboard'
         ],
@@ -37,7 +42,7 @@ const navigations = ref([
     {
         name: 'quiz-list',
         text: 'Quizes',
-        icon: 'test.png',
+        icon: test,
         names: [
             'quiz-list',
             'quiz-create',
@@ -48,7 +53,7 @@ const navigations = ref([
     {
         name: 'files',
         text: 'Files',
-        icon: 'folder.png',
+        icon: folder,
         names: [
             'files'
         ],
