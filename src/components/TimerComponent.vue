cf713112eefa2e7bcf57817cf441a44fd9ae5bb5
haparece <harvey@fulloutsoftware>
2023-09-10 13:41:10 +0800

    Added timer, not yet complete, needed animation

---
+++ src/components/TimerComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="w-28 h-28 flex items-center relative">
+        <div class="w-24 h-24 absolute rounded-full z-10 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div class="w-24 h-12 bg-blue-500 rounded-t-full"></div>
+            <div class="w-24 h-12 bg-blue-500 rounded-b-full"></div>
+        </div>
+        <div class="w-24 h-24 rounded-full bg-transparent mx-auto flex items-center relative z-20">
+            <div class="w-20 h-20 rounded-full bg-green-200 mx-auto flex items-center text-center">
+                <span class="block mx-auto">00:00</span>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/Pages/Quizes/QuizPreview.vue
+++ src/views/Pages/Quizes/QuizPreview.vue
@@ -1,41 +1,46 @@
 <template>
     <div class="w-full h-screen overflow-y-auto bg-gradient-to-r from-[#d5ecf1] to-[#dcfddb] grid items-center px-4">
-        <div class="p-5 my-5 rounded mx-auto bg-slate-50 min-w-[320px] w-full max-w-[1200px]">
-            <strong class="py-5 text-lg">{{ quiz.title }}</strong>
-            <b class="py-5" v-html="quiz.instruction"></b>
+        <div class="p-5 my-5 rounded mx-auto bg-slate-50 min-w-[320px] w-full max-w-[1200px] h-auto max-h-[calc(100vh-40px)]">
+            <div class="flex justify-between items-center">
+                <div>
+                    <strong class="py-5 text-lg">{{ quiz.title }}</strong>
+                    <b class="py-5" v-html="quiz.instruction"></b>
+                </div>
+                <div class="bg-sky-200 shadow">
+                    <TimerComponent />
+                </div>
+            </div>
             <hr class="my-5">
 
-            <div class="questions">
+            <div class="questions h-auto max-h-[calc(100vh-250px)] overflow-y-auto">
                 <div class="p-5 bg-white mx-2 my-4 rounded-md shadow-sm" v-for="(question) in questions" :key="question.key">
-                    <strong>{{ question.description }}</strong>
-                    <div class="p-2" v-if="question.type == ENUMERATION">
-                        <form action="" class="flex items-center space-x-2">
-                            <input type="text" class="border p-2 rounded-md block w-[100%] focus:outline outline-gray-200 outline-1">
-                            <input type="submit" class="bg-blue-500 text-white px-2 py-2 rounded cursor-pointer"
-                                        value="&plus; Add">
-                        </form>
+                    <div class="flex items-center space-x-2">
+                        <img src="../../../assets/cirlcle.png" class="w-5 h-5 bg-gray-100 rounded-full" alt="">
+                        <strong class="flex-1">{{ question.description }}</strong>
 
-                        <div class="answer-pool">
-                            <code>TODO: need to decide how to answer in enumeration type questions</code>
-                        </div>
+                        <button class="border px-4 py-2 rounded hover:bg-gray-300 hover:text-white">clear</button>
+                    </div>
+                    <div class="p-2" v-if="question.type == ENUMERATION">
+                        <small class="text-yellow-500">NOTE: Please separate the answers with comma "," (<i>Ex. months, years</i>)</small><br>
+                        <input type="text" class="border p-2 rounded-md block w-[100%] focus:outline outline-gray-200 outline-1" disabled>
                     </div>
 
                     <div class="p-2" v-if="question.type == SELECT">
                         <div class="flex space-x-2 py-2" v-for="(choice, index) of JSON.parse(question.choices)" :key="index">
-                            <input type="checkbox" name="answer" id="" :value="index">
+                            <input type="checkbox" name="answer" id="" :value="index" disabled>
                             <label for="" class="flex-1">{{ choice }}</label>
                         </div>
                     </div>
 
                     <div class="p-2" v-if="question.type == MULTIPLE_CHOICE">
                         <div class="flex space-x-2 py-2" v-for="(choice, index) of JSON.parse(question.choices)" :key="index">
-                            <input type="radio" name="answer" id="" :value="index">
+                            <input type="radio" name="answer" id="" :value="index" disabled>
                             <label for="" class="flex-1">{{ choice }}</label>
                         </div>
                     </div>
 
                     <div class="p-2" v-if="question.type == EXPLANATION">
-                        <textarea placeholder="Answer here" name="" id="" cols="30" rows="4" class="box-border p-4 w-full border rounded focus:outline outline-gray-200 outline-1"></textarea>
+                        <textarea placeholder="Answer here" name="" id="" cols="30" rows="4" class="box-border p-4 w-full border rounded focus:outline outline-gray-200 outline-1 resize-none" disabled></textarea>
                     </div>
 
                     <div class="p-2" v-if="question.type == TRUE_OR_FALSE">
@@ -63,6 +68,9 @@ import { onMounted, ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { useQuizStore } from '@/stores/quiz';
 import { TRUE_OR_FALSE, MULTIPLE_CHOICE, ENUMERATION, EXPLANATION, SELECT } from '../../../utils/const'
+
+// component
+import TimerComponent from '@/components/TimerComponent.vue';
 
 const route = useRoute()
 const router = useRouter()
